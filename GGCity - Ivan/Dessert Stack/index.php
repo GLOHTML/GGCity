7416261e72f4c8e2182f1f6356a14e4980d9f351
Ivan Cahyakusuma <ivancahya.gamelevelone@gmail.com>
2020-07-24 10:35:30 +0700

    fix disabled

--- GGCity - Ivan/Dessert Stack/index.php
+++ GGCity - Ivan/Dessert Stack/index.php
@@ -1,6 +1,6 @@
 <?php
     $dir = str_replace(basename($_SERVER["SCRIPT_FILENAME"]),"",$_SERVER["SCRIPT_NAME"]);
-    $_GET['dom']=$_SERVER['SERVER_NAME'];
+    $_GET['dom']=$_SERVER['HTTP_REFERER'];
     include_once($_SERVER['DOCUMENT_ROOT']."/includegl1/CheckAllow.php");
     if(!$playallow){
         header("location:/unavailable.html");
